655af54e5fc6010d9b9190f799af715aeb71cebc
Jeremy Winchester <jeremy.c.winchester@gmail.com>
2024-12-04 22:37:51 +0000

    day 3, second star

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -11,28 +11,57 @@ def read_file_content(file_path: Path) -> str:
     return file_path.read_text(encoding="utf-8")
 
 
-def parse_mul_expressions(line: str) -> List[str]:
-    return re.findall(r"mul\(\d{1,3},\d{1,3}\)", line)
+def extract_mul(line: str) -> int:
+    return [
+        (match.start(), int(match.group(1)), int(match.group(2)))
+        for match in re.finditer(r"mul\((\d+),(\d+)\)", line)
+    ]
 
 
-def extract_and_multiply(expressions: List[str]) -> int:
-    return sum(
-        int(left) * int(right)
-        for expr in expressions
-        if (match := re.search(r"(\d+),(\d+)", expr))
-        for left, right in [match.groups()]
-    )
+def extract_do_and_dont(
+    line: str,
+) -> int:
+    return [
+        (match.start(), match.group()) for match in re.finditer(r"don't\(\)", line)
+    ] + [(match.start(), match.group()) for match in re.finditer(r"do\(\)", line)]
 
 
 def process_line(line: str) -> int:
-    expressions = parse_mul_expressions(line)
-    return extract_and_multiply(expressions)
+    mul_expresions = extract_mul(line)
+    return sum([left * right for _, left, right in mul_expresions])
+
+
+def sum_based_on_instructions(merged: List[tuple]) -> int:
+    result = 0
+    enabled = True
+    for x in merged:
+        if len(x) == 3:
+            if enabled:
+                _, left, right = x
+                result += left * right
+        else:
+            _, instruction = x
+            if instruction == "don't()":
+                enabled = False
+            elif instruction == "do()":
+                enabled = True
+    return result
+
+
+def process_line_pt2(line: str) -> int:
+    mul_expresions = extract_mul(line)
+    do_and_dont = extract_do_and_dont(line)
+    merged = sorted(mul_expresions + do_and_dont, key=lambda x: x[0])
+    return sum_based_on_instructions(merged)
 
 
 def main() -> None:
     file_content = read_file_content(file_path)
     result = process_line(file_content)
     print(f"Result: {result}")
+
+    result_pt2 = process_line_pt2(file_content)
+    print(result_pt2)
 
 
 if __name__ == "__main__":
