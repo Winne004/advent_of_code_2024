ea702147483ae886bb9d6a166bfd7a4fef33efcd
Jeremy Winchester <jeremy.c.winchester@gmail.com>
2024-12-08 11:38:26 +0000

    day 6 pt 1 done

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -2,7 +2,7 @@ from itertools import cycle
 from pathlib import Path
 from collections import Counter
 
-file_name = "day_6_test_input.txt"
+file_name = "day_6_input.txt"
 script_dir = Path(__file__).parent
 file_path = script_dir / file_name
 
@@ -11,8 +11,8 @@ def read_file_content(file_path: Path) -> str:
     return file_path.read_text(encoding="utf-8")
 
 
-class Directions:
-    def __init__(self):
+class Guard:
+    def __init__(self, grid):
         self.current_direction = 0
         self.directions = [
             self.go_up,
@@ -21,66 +21,67 @@ class Directions:
             self.go_left,
         ]
         self.trace = set()
+        self.grid = grid
+        self.x, self.y = self.find_starting_point(grid)
+        self.previous_step = None
 
-    def go_down(self, x, y):
-        return (x, y + 1)
+    def go_down(self):
+        self.y += 1
 
-    def go_up(self, x, y):
-        return (x, y - 1)
+    def go_up(
+        self,
+    ):
+        self.y -= 1
 
-    def go_left(self, x, y):
-        return (x - 1, y)
+    def go_left(
+        self,
+    ):
+        self.x -= 1
 
-    def go_right(self, x, y):
-        return (x + 1, y)
+    def go_right(
+        self,
+    ):
+        self.x += 1
 
     def turn_right(self):
         self.current_direction = (self.current_direction + 1) % len(self.directions)
 
-    def generate_coords(self, x, y):
-        return self.directions[self.current_direction](x, y)
+    def generate_coords(self):
+        self.directions[self.current_direction]()
+        return self.x, self.y
 
-    def yield_directions(self, x, y):
-        for _ in range(4):
-            yield self.generate_coords(x, y)
+    def find_starting_point(self, grid):
+        for y, row in enumerate(grid):
+            for x, cell in enumerate(row):
+                if cell == "^":
+                    self.trace.add((x, y))
+                    return x, y
+        return None
 
+    def in_bounds(self):
+        return 0 <= self.x < len(self.grid[0]) and 0 <= self.y < len(self.grid)
 
-def in_bounds(x, y, grid):
-    return 0 <= x < len(grid[0]) and 0 <= y < len(grid)
+    def is_obstructed(self):
+        return self.grid[self.y][self.x] == "#"
 
-
-def navigate(x, y, grid, directions):
-    if not in_bounds(x, y, grid):
-        return 1
-
-    if grid[y][x] == "#":
-        directions.turn_right()
-        return 0
-
-    for tmp_x, tmp_y in directions.yield_directions(x, y):
-
-        tmp_x, tmp_y = directions.generate_coords(x, y)
-        res = navigate(tmp_x, tmp_y, grid, directions)
-        if res:
-            print(f"({tmp_x}, {tmp_y})")
-            return res + 1
-
-
-def find_starting_point(grid):
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == "^":
-                return x, y
-    return None
+    def navigate(self):
+        while self.in_bounds():
+            if self.is_obstructed():
+                self.x, self.y = self.previous_step
+                self.turn_right()
+            else:
+                self.previous_step = (self.x, self.y)
+                self.trace.add((self.x, self.y))
+                print(self.x, self.y)
+            self.generate_coords()
 
 
 def main() -> None:
-    map = read_file_content(file_path).splitlines()
 
-    x, y = find_starting_point(map)
-    directions = Directions()
-    result = navigate(x, y, map, directions)
-    print(result)
+    grid = read_file_content(file_path).splitlines()
+    directions = Guard(grid=grid)
+    directions.navigate()
+    print(len(directions.trace) + 1)
 
 
 if __name__ == "__main__":
