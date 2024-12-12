d422616720a35cf0336e3cdb927adef04a7b9829
Jeremy Winchester <jeremy.c.winchester@gmail.com>
2024-12-12 12:48:00 +0000

    day 10 done

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -59,15 +59,51 @@ def find_trail_head(grid, x, y, search_string, move_directions, seen):
     return res
 
 
-def main():
+def record_step(path, x, y):
+    return path + str(x) + str(y)
+
+
+def find_distinct_trail_head(grid, x, y, search_string, path, move_directions, seen):
     res = 0
+
+    path = record_step(path, x, y)
+
+    if not in_bounds(x, y, grid):
+        return 0
+
+    if grid[y][x] == "9" and search_string == "9" and path not in seen:
+        seen.add(path)
+        return 1
+
+    if not search_string or grid[y][x] != search_string[0]:
+        return 0
+
+    path = record_step(path, x, y)
+
+    for direction in directions:
+
+        x_offset, y_offset = direction(x, y)
+        res += find_distinct_trail_head(
+            grid, x_offset, y_offset, search_string[1:], path, move_directions, seen
+        )
+    return res
+
+
+def main():
     trail_map = read_file_content(file_path).splitlines()
+    res_pt_1 = 0
+    res_pt_2 = 0
     for y, row in enumerate(trail_map):
         for x, cell in enumerate(row):
             if cell == "0":
-                res += find_trail_head(trail_map, x, y, "0123456789", directions, set())
+                res_pt_1 += find_trail_head(
+                    trail_map, x, y, "0123456789", directions, set()
+                )
+                res_pt_2 += find_distinct_trail_head(
+                    trail_map, x, y, "0123456789", "", directions, set()
+                )
 
-    print(res)
+    print(f"Part 1: {res_pt_1}\nPart 2: {res_pt_2}")
 
 
 if __name__ == "__main__":
